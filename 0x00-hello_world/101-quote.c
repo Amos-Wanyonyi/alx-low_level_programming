f28e61ac75df4dbe7975fcf44b8e82c52c88f9b4
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-15 10:22:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * Description: main - A program in Unix.
+ * main - Entry point.
  * Return: 1 if success.
  */
 int main(void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",
 	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
