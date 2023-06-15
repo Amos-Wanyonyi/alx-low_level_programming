2c89a34f0e30b2b67ace14c9c20bb073f466cc87
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-15 07:28:39 -0700

    Intel

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/*
+ * main- A program that prints a statement followed by a new line
+ * return 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
