81d0d24f0bba0cc67153cd031d2edc18a2f21ced
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-15 09:24:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: A program that prints a sentence
- * return 0 (success)
+ * Description: main - A program that prints a sentence.
+ * Return: 0 if success.
  */
 int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
