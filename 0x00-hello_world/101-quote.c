f32c507c4b662c221d88fa58ae1c6a8d8e0b9f31
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-15 09:28:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistad.h>
 
-/*
- * main - A program in Unix
- * Return 1 (success)
+/**
+ * Descroption: main - A program in Unix.
+ * Return: 1 if success.
  */
 int main(void)
+{
 	fput("and that piece of art is useful\n" - Dora Korpar, 2015-10-19 \n", stdout);
 	return (1);
 }
