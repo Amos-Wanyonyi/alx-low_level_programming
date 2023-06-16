ff721414e9a585d9bb0df7c1df3f8407e2ce9bf8
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-16 07:07:16 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 0; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	return (0);
+}
