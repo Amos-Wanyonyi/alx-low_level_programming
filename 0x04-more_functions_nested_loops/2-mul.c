4b4c528eece2a9b0348b7d6a3a2b60d15dd22a03
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-22 09:48:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplication of intergers
+ * @a: variable 1
+ * @b: variable 2
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
