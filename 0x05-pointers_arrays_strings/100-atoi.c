de0adcc4f8eea8b264149f8d1d53a8dd8915f8d4
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-06-28 05:30:14 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,35 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 /**
   * _atoi- convert a string to an integer.
-  * @s: string input
-  * Return: int number
-**/
+  * @s: string to convert
+  * Return: The converted string
+  **/
 int _atoi(char *s)
 {
-	int number2 = 0, size = 0, negatives = 0, p = 1, j, start = 0, end;
-	unsigned int number = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-	if (s[0] == '\0')
-	{ return (number2); }
-	while (s[size] != '\0')
+	while (s[i] == ' ')
+		i++;
+	if (s[i] == '+' || s[i] == '-')
 	{
-	size++;
+		sign = (s[i] == '-') ? -1 : 1;
+		i++;
 	}
-	while (s[start] < 48 || s[start] > 57)
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-	start++;
-		if (start == size)
-		{ return (number2); }
+		if (result > (INT_MAX - (s[i] - '0')) / 10)
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+		result = result * 10 + (s[i] - '0');
+		i++;
 	}
-	end = start;
-	while (s[end + 1] > 47 && s[end + 1] < 58)
-	{
-	end++;
-	}
-	for (j = 0; j < start; j++)
-	{
-	if (s[j] == 45)
-	{ negatives++; }
-	}
-	for (j = end; j >= start; j--)
-	{
-	number = number + (s[j] - '0') * p;
-	if (p < 1000000000)
-	{ p = p * 10; }
-	}
-	if (number == 2147483648 && (negatives % 2 != 0))
-	{ number2 = -2147483648; }
-	else
-	{ number2 = number;
-	if (negatives % 2 != 0)
-	{ number2 = -number2; }
-	}
-return (number2);
+	return (sign * result);
 }
