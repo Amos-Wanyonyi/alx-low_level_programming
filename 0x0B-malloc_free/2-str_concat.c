1a1782d20402caf56703c2be74091613258b9041
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-07-10 10:30:59 -0700

    101-strtow.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,28 +10,24 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, size1, size2 = 0;
 	char *p;
+	int i, size1 = 0, size2 = 0;
 
 	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+	{ s1 = ""; }
 	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+	{ s2 = ""; }
 	while (s1[size1] != '\0')
 	{
-		size1++;
+	size1++;
 	}
 	while (s2[size2] != '\0')
 	{
-		size2++;
+	size2++;
 	}
 	p = malloc((size1 + size2 + 1) * sizeof(char));
-		if (p == NULL)
-			return (0);
+	if (p == NULL)
+	return (0);
 	for (i = 0; i < size1; i++)
 	{
 		p[i] = s1[i];
@@ -41,5 +37,5 @@ char *str_concat(char *s1, char *s2)
 		p[i] = s2[i - size1];
 	}
 	p[i] = '\0';
-	return (0);
+return (p);
 }
