eee5e95f215505153cd671d0ee997e7fdeb486ef
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-07-18 06:55:13 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,10 +11,9 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 /* Other function prototypes */
 int _putchar(char c);
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
