ece3d281277fc1f98f9eabefb51f9fd525f634e0
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-07-18 10:47:04 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <string.h>
+/**
+  * _strdup - returns a pointer to a newly allocated space in memory
+  * contains a copy of the string given as a parameter.
+  * @str: input array
+  * Return: pointer to array
+  */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, size = 0;
+
+	if (!str)
+		return (0);
+	while (str[size] != '\0')
+	{
+	size++;
+	}
+	p = malloc((size + 1) * sizeof(char));
+	if (p == NULL)
+	return (p);
+	for (i = 0; i < size; i++)
+	{
+	p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
+}
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of owner of the dog
- * Return: pointer to the new dog
- */
+* new_dog - check the code for Holberton School students.
+* @name: name's dog
+* @age: age's dog
+* @owner: owner's dog
+* Return: new dog pointer
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+	dog_t *new;
 
-	new_dog = malloc(sizeof(dog_t));
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+	return (NULL);
+	new->name = _strdup(name);
+	if (new->name == 0)
+	free(new);
+	new->age = age;
+	new->owner = _strdup(owner);
+	if (new->owner == 0)
 	{
-		if (new_dog == NULL)
-			return (NULL);
+		if (new->name != 0)
+		free(new->name);
+		free(new);
 	}
-
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
-
-	if (name_copy == NULL || owner_copy == NULL)
-	{
-		free(name_copy);
-		free(owner_copy);
-		free(new_dog);
-		return (NULL);
-	}
-	
-	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner = owner_copy;
-
-	return (new_dog);
+return (new);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
  * free_dog - frees the memory allocated for dog
  * @d: pointer to the freed dog
@@ -8,7 +10,6 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-
 	free(d->name);
 	free(d->owner);
 	free(d);
