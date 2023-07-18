0916fe25204ace49fc307f4187106e954c68a2b7
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-07-18 09:48:29 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - frees the memory allocated for dog
+ * @d: pointer to the freed dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
