38addb6a20f0d2c109130b79594122249caf490a
Amos Wanyonyi <wamalabeamos@gmail.com>
2023-07-21 03:37:15 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,17 +10,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp && array)
+	if (!(cmp && array) || size <= 0)
 	{
-		if (size <= 0)
-		{
-			return (-1);
-		}
+		return (-1);
 	}
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i]) != 0)
+		{
 			return (i);
+		}
 	}
 	return (-1);
 }
