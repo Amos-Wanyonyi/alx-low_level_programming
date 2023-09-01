d390fc597061bd63098fafb42a432277fb9102c0
Amos-Wanyonyi <wamalabeamos@gmail.com>
2023-09-01 07:14:13 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This is a Static libraries readme file
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char c)
+{
+	return (_putchar(c));
+}
