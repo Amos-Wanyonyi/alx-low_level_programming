1897bebba9903b7003249046b1349e03ce8a1bcb
Amos-Wanyonyi <wamalabeamos@gmail.com>
2023-09-01 09:19:46 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,18 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
 	(void)argv;
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i++)
-			;
-		printf("%d\n", i - 1);
-	}
-	else
-	{
-		printf("0\n");
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
