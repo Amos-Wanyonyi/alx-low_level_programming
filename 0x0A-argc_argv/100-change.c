348512c06d99f0c148d60aa3928d2c06b83471c5
Amos-Wanyonyi <wamalabeamos@gmail.com>
2023-09-02 00:04:42 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,42 +8,28 @@
 */
 int main(int argc, char *argv[])
 {
-int change = 0, n;
+	int change, n;
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-n = atoi(argv[1]);
-if (n < 0)
-{
-printf("0\n");
-return (0);
-}
-if (n >= 25)
-{
-change = n / 25;
-n = n % 25;
-}
-if (n >= 10)
-{
-change = n / 10;
-n = n % 10;
-}
-if (n >= 5)
-{
-change = n / 5;
-n = n % 5;
-}
-if (n >= 2)
-{
-change = n / 2;
-n = n % 2;
-}
-change += n;
-{
-printf("%d\n", change);
-return (0);
-}
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	change = atoi(argv[1]);
+	for (n = 0; change > 0; n++)
+	{
+		if (change - 25 >= 0)
+			change -= 25;
+		else if (change - 10 >= 0)
+			change -= 10;
+		else if (change - 5 >= 0)
+			change -= 5;
+		else if (change - 2 >= 0)
+			change -= 2;
+		else if (change - 1 >= 0)
+			change -= 1;
+	}
+	printf("%d\n", n);
+	return (0);
 }
