dd7a7f2fdd6d0eeb60699bd76f4f83a39c0e2e15
Amos Wanyonyi <wamalabeamos@gmail>
2023-12-19 11:05:00 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: is the name of the file to be created
+ * @text_content: the contens of the new file
+ * Return: 1 if success or 0 if fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content,  len);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
