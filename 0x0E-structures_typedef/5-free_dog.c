f4442843469072e997a12b0b149779832197ae99
Amos Wanyonyi <wamalabeamos@gmail>
2023-12-25 06:37:41 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees the dog
+ * @d: the struct to free dogs
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
